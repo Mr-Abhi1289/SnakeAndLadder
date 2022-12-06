fed64d96fee3d2201640a63f53824cf5eaf56f26
unknown <abhimvj575@gmail.com>
2022-12-06 20:21:53 +0530

    [Abhilash] Game starts at player position 0

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        public const int StartPosition = 0, FinishPosition = 100;
+        static void Main(string[] args)
+        {
+            int currentPosition = StartPosition;
+            Console.WriteLine("Player Statrting Position="+StartPosition);
+        }
+    }
+}
