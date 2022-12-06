bfa51fb396845c7e719a8bd6196f55e508adcad0
unknown <abhimvj575@gmail.com>
2022-12-06 20:40:57 +0530

    [Abhilash] The Player rolls the die to get a number between 1 to 6

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -1,14 +1,22 @@
 ﻿using System;
+using System.Security;
 
 namespace SnakeAndLadder
 {
     class Program
     {
-        public const int StartPosition = 0, FinishPosition = 100;
+        public const int START_POS = 0, FINISH_POS = 100;
+        public static Random random = new Random();
+        public static int DiceRoll()
+        {
+            return random.Next(1, 6);
+        }
         static void Main(string[] args)
         {
-            int currentPosition = StartPosition;
-            Console.WriteLine("Player Statrting Position="+StartPosition);
+            int currentPosition = START_POS;
+            int numOnDice = DiceRoll();
+            Console.WriteLine("number on the dice=" + numOnDice);
+            Console.WriteLine("player poasition="+currentPosition);
         }
     }
 }
