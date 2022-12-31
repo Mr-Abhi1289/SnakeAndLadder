1377ba2f5a8d046b8d41a4df2cf4d6ef42024efb
unknown <abhimvj575@gmail.com>
2022-12-31 21:47:18 +0530

    [Abhilash] Added:Play the game with 2 players

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -2,10 +2,28 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            StartGame SG= new StartGame();
-            SG.RollDie();
+        
+            static void Main(string[] args)
+            {
+                StartGame SG = new StartGame();
+                int player1 = SG.RollDie();
+                StartGame SG1 = new StartGame();
+                int player2 = SG1.RollDie();
+                Console.WriteLine("Player 1  total number dice roll is  " + player1);
+                Console.WriteLine("Player 2  total number dice roll is  " + player2);
+                int num1 = player1.CompareTo(player2);
+                if (num1 == 0)
+                {
+                    Console.WriteLine("Game is Tie");
+                }
+                if (num1 == 1)
+                {
+                    Console.WriteLine("Player 1 Wins the match");
+                }
+                else
+                {
+                    Console.WriteLine("Player 2 Wins the match");
+                }
+            }
         }
     }
-}
