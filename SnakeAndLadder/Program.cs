d0c34bc0fec729aff61c54b39808036b8b3176b5
unknown <abhimvj575@gmail.com>
2022-12-31 20:58:31 +0530

    [Abhilash] Added:Snake and Ladder game played with single player at start position 0

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -1,22 +1,11 @@
-﻿using System;
-using System.Security;
-
-namespace SnakeAndLadder
+﻿namespace SnakeAndLadder
 {
-    class Program
+    internal class Program
     {
-        public const int START_POS = 0, FINISH_POS = 100;
-        public static Random random = new Random();
-        public static int DiceRoll()
-        {
-            return random.Next(1, 6);
-        }
         static void Main(string[] args)
         {
-            int currentPosition = START_POS;
-            int numOnDice = DiceRoll();
-            Console.WriteLine("number on the dice=" + numOnDice);
-            Console.WriteLine("player poasition="+currentPosition);
+            StartGame SG= new StartGame();
+            SG.Gamestart();
         }
     }
 }
---
+++ SnakeAndLadder/StartGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class StartGame
+    {
+        
+        public void Gamestart()
+        {
+            int PlayerPosition = 0;
+            Console.WriteLine("Game is Start Player is at start position:" + PlayerPosition);
+        }
+
+    }
+    
+}
