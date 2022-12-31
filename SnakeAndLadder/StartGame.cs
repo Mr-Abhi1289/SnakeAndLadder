35b9165651b8c4c560a0d6e166bcc67e8505b43b
unknown <abhimvj575@gmail.com>
2022-12-31 21:12:52 +0530

    [Abhilash] Added:The Player then checks for a Option. They are No Play,Ladder or Snake.

--- SnakeAndLadder/StartGame.cs
+++ SnakeAndLadder/StartGame.cs
@@ -9,12 +9,32 @@ namespace SnakeAndLadder
     internal class StartGame
     {
 
-
+        int Position = 0;
         Random random = new Random();
+       
+
         public void RollDie()
         {
-            int dice = random.Next(1, 7);
-            Console.WriteLine(dice);
+
+            int num = random.Next(1, 7);
+
+
+            int num1 = random.Next(0, 3);
+            switch (num1)
+
+            {
+                case 0:
+                    Console.WriteLine("Player is in the same position");
+                    break;
+                case 1:
+                    Position += num;
+                    Console.WriteLine("Player position is : " + Position);
+                    break;
+                case 2:
+                    Position -= num;
+                    Console.WriteLine("Player position is : " + Position);
+                    break;
+            }
         }
 
     }
